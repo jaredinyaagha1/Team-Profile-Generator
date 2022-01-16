445156e39d99e22e41fe9bc677a880ee120ccbb0
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-01-16 15:23:34 -0500

    finished index file

--- index.js
+++ index.js
@@ -48,7 +48,107 @@ function init() {
 
             teamArray.push(manager);
         });
-}
+};
+
+const addMember = () => {
+    inquirer
+        .prompt([{
+                type: 'list',
+                name: 'role',
+                message: "Please choose your employee's role",
+                choices: ['Engineer', 'Intern']
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the Employee's name? ",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Employee's id? ",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the Employee's email? ",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Please enter the employee's github username.",
+                when: (input) => input.role === "Engineer"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Please enter the intern's school.",
+                when: (input) => input.role === "Intern"
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddEmployee',
+                message: "Would you like to add more team members?",
+                default: false
+            }
+        ])
+        .then(employeeInfo => {
+            // data 
+
+            let {
+                name,
+                id,
+                email,
+                role,
+                github,
+                school,
+                confirmAddEmployee
+            } = employeeInfo;
+            let employee;
+
+            if (role === "Engineer") {
+                employee = new Engineer(name, id, email, github);
+
+                console.log(employee);
+            } else if (role === "Intern") {
+                employee = new Intern(name, id, email, school);
+
+                console.log(employee);
+            }
+
+            teamArray.push(employee);
+
+            if (confirmAddEmployee) {
+                return addEmployee(teamArray);
+            } else {
+                return teamArray;
+            }
+        })
+};
 
 
-init();
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+            // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+};
+
+init()
+    .then(addMember)
+    .then(teamArray => {
+        return generateHTML(teamArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
