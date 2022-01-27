259522d1bd5d2e6b18465822193fa088444eceda
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-01-26 23:07:22 -0500

    created intern.test.js and manager.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,36 @@
 const Intern = require('../lib/Intern');
+
+test(" properly creates employee object", () => {
+    const e = new Intern();
+    expect(typeof(e)).toBe("object")
+});
+
+test("able to set name via constructor argument", () => {
+    const testName = "Michael";
+    const e = new Intern(testName);
+    expect(e.name).toBe(testName);
+});
+
+test("able to set id via constructor argument", () => {
+    const testId = 777;
+    const e = new Intern("FooB", testId);
+    expect(e.id).toBe(testId);
+});
+
+test("able to set email via constructor argument", () => {
+    const testEmail = "michael@pascuzzi.com";
+    const e = new Intern("Boofar", 777, testEmail);
+    expect(e.email).toBe(testEmail)
+});
+
+test("able to get role via getRole()", () => {
+    const testRole = "Intern";
+    const e = new Intern("Boofar", 777, testRole);
+    expect(e.role).toBe(testRole);
+});
+
+test("able to get School via getSchool()", () => {
+    const testSchool = "Georgia Institute of Technology";
+    const e = new Intern("Foofar", 777, testSchool);
+    expect(e.github).toBe(testSchool);
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,36 @@
 const Manager = require('../lib/Manager');
+
+test(" properly creates employee object", () => {
+    const e = new Engineer();
+    expect(typeof(e)).toBe("object")
+});
+
+test("able to set name via constructor argument", () => {
+    const testName = "Michael";
+    const e = new Engineer(testName);
+    expect(e.name).toBe(testName);
+});
+
+test("able to set id via constructor argument", () => {
+    const testId = 777;
+    const e = new Engineer("FooB", testId);
+    expect(e.id).toBe(testId);
+});
+
+test("able to set email via constructor argument", () => {
+    const testEmail = "michael@pascuzzi.com";
+    const e = new Engineer("Boofar", 777, testEmail);
+    expect(e.email).toBe(testEmail)
+});
+
+test("able to get role via getRole()", () => {
+    const testRole = "Engineer";
+    const e = new Engineer("Boofar", 777, testRole);
+    expect(e.role).toBe(testRole);
+});
+
+test("able to get OfficeNumber via getOfficeNumber()", () => {
+    const testOfficeNumber = "9999999999";
+    const e = new Engineer("Foofar", 777, testOfficeNumber);
+    expect(e.github).toBe(testOfficeNumber);
+});
